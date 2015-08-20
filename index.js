b6cdadb349e0cf26a27a1942215e5383ee1ee098
Roman Getman <romangetman@gmail.com>
2015-08-20 19:12:14 +0300

    Load posts from database

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ import ReactDOMServer from 'react-dom/server'
 import {Router} from 'react-router';
 import MemoryHistory from 'react-router/lib/MemoryHistory';
 import React from 'react';
+import knex from 'knex';
+import bookshelf from 'bookshelf';
 
 import routes from './src/routing';
 import ApiController from './src/api/controller'
+
+let my_knex = knex({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'libertysoil',
+    password : '',
+    database : 'libertysoil',
+    charset  : 'utf8'
+  }
+});
+
+let my_bookshelf = bookshelf(my_knex);
 
 let wrap = fn => (...args) => fn(...args).catch(args[2]);
 let app = express();
@@ -21,9 +36,9 @@ app.use(function(req, res, next) {
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-let controller = new ApiController();
+let controller = new ApiController(my_knex);
 app.get('/api/v1/test', wrap(controller.test));
-app.get('/api/v1/posts', wrap(controller.posts));
+app.get('/api/v1/posts', wrap(controller.posts.bind(controller)));
 
 app.use(express.static('public', { index: false}));
 app.use((req, res, next) => {
