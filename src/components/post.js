77c8e8addbd729a9883762c934a75da0baaaec3c
Roman Getman <romangetman@gmail.com>
2015-08-20 17:02:13 +0300

    Use actual post template

--- src/components/post.js
+++ src/components/post.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 export class TextPostComponent extends React.Component {
   render() {
-    return (<p>{this.props.text}</p>)
+    return (
+      <section className="card">
+        <header className="card__header">
+          Psychology today
+        </header>
+        <div className="card__content">
+          <p>{this.props.text}</p>
+        </div>
+        <div className="card__owner">
+          <section className="user_box">
+            <a href="#" className="user_box__name">User Name</a>
+
+            <div className="user_box__text">yesterday at 8:01</div>
+          </section>
+        </div>
+        <footer className="card__footer">
+          <div className="tags">
+            <span className="tag">Psychology</span>
+            <span className="tag">Gaming</span>
+          </div>
+          <div className="card__toolbar">
+            <span className="icon fa fa-heart-o"></span>
+            <span className="icon fa fa-star-o"></span>
+          </div>
+        </footer>
+      </section>
+    )
   }
 }
