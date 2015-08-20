dc4df4dd6e5ea2a8ea09e6d7579ec9b752b6b32a
Alexey Zakhlestin <indeyets@gmail.com>
2015-08-20 12:37:23 +0300

    bootstrapped react

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,34 @@
-console.info(42);
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {Router, Route, Link} from 'react-router';
+import {history} from 'react-router/lib/BrowserHistory';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello, world!</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is an index</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Router history={history}>
+    <Route component={App}>
+      <Route component={Index} name="index" path="/" />
+    </Route>
+  </Router>,
+  document.getElementById('content')
+);
