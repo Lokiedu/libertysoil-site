4d7deec021e43f9cb14d2e6ffadc9d8a21bb766d
Alexey Zakhlestin <indeyets@gmail.com>
2015-08-20 18:43:07 +0300

    initial db schema

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id uuid not null unique PRIMARY KEY,
+    username text not null unique,
+    hashed_password text not null,
+    email text not null unique,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    more jsonb
+);
+
+CREATE TABLE posts (
+    id uuid not null unique PRIMARY KEY,
+    user_id uuid not null REFERENCES users (id) ON DELETE CASCADE,
+    "text" text not null,
+    type text,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    more jsonb
+);
