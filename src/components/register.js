d30a10ee0563b6ab8070c27831cd0cff2ca340a7
Roman Getman <romangetman@gmail.com>
2015-08-21 14:15:00 +0300

    Try sending registration data

--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,44 @@
 import React from 'react'
 
 export default class RegisterComponent extends React.Component {
+
+  async submitHandler(event) {
+
+    event.preventDefault();
+
+    let form = event.target;
+
+    if(true || form.password.value != '' && form.password.value === form.password_repeat.value) {
+      const host = 'http://localhost:8000';
+
+      let data = new FormData(form);
+      data.append('user', 'hubot');
+
+      //console.log(data.getAll());
+
+      let result = await fetch(`${host}/api/v1/users`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: 'Hubot',
+          login: 'hubot',
+        })
+
+      });
+
+
+
+    } else {
+      //Error
+      console.log('error');
+    }
+
+
+    //this.setState({message: event.target.value});
+  };
+
   render() {
     return (
       <div className="page__body">
@@ -27,33 +65,35 @@ export default class RegisterComponent extends React.Component {
             </div>
             <div className="box box-middle">
               <header className="box__title">Register</header>
-              <div className="box__body">
-                <div className="layout__row">
-                  <div className="form__row">
-                    <label className="label label-block">First name</label>
-                    <input className="input" type="text"/>
+              <form action="" onSubmit={this.submitHandler}>
+                <div className="box__body">
+                  <div className="layout__row">
+                    <div className="form__row">
+                      <label className="label label-block">First name</label>
+                      <input className="input" type="text" name="username"/>
+                    </div>
+                    <div className="form__row">
+                      <label className="label label-block">Last name</label>
+                      <input className="input" type="text"/>
+                    </div>
+                    <div className="form__row">
+                      <label className="label label-block">Email</label>
+                      <input className="input" type="email" name="email"/>
+                    </div>
+                    <div className="form__row">
+                      <label className="label label-block">Password</label>
+                      <input className="input" type="password" name="password"/>
+                    </div>
+                    <div className="form__row">
+                      <label className="label label-block">Repeat password</label>
+                      <input className="input" type="password" name="password_repeat"/>
+                    </div>
                   </div>
-                  <div className="form__row">
-                    <label className="label label-block">Last name</label>
-                    <input className="input" type="text"/>
-                  </div>
-                  <div className="form__row">
-                    <label className="label label-block">Email</label>
-                    <input className="input" type="email"/>
-                  </div>
-                  <div className="form__row">
-                    <label className="label label-block">Password</label>
-                    <input className="input" type="password"/>
-                  </div>
-                  <div className="form__row">
-                    <label className="label label-block">Repeat password</label>
-                    <input className="input" type="password"/>
+                  <div className="layout__row layout layout-align_vertical layout-align_justify">
+                    <button className="button button-yellow">Send</button>
                   </div>
                 </div>
-                <div className="layout__row layout layout-align_vertical layout-align_justify">
-                  <button className="button button-yellow">Login</button>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
