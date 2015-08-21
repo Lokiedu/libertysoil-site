9e4406f14a9a8ec6bb4b395474e678d6715e6ec8
Roman Getman <romangetman@gmail.com>
2015-08-21 15:59:56 +0300

    Update registration component

--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import request from 'superagent';
-export default class RegisterComponent extends React.Component {
+import { connect } from 'react-redux';
+
+import store, {addError} from '../store';
+
+class RegisterComponent extends React.Component {
 
   async submitHandler(event) {
 
@@ -22,71 +26,52 @@ export default class RegisterComponent extends React.Component {
 
     } else {
       //Error
+      store.dispatch(addError({message: 'Passwords do not match'}));
     }
 
-
-    //this.setState({message: event.target.value});
   };
 
   render() {
     return (
-      <div className="page__body">
-        <div className="area">
-          <div className="area__body layout-align_start">
-            <div className="box box-middle">
-              <header className="box__title">Login</header>
-              <div className="box__body">
-                <div className="layout__row">
-                  <div className="form__row">
-                    <label className="label label-block">Email</label>
-                    <input className="input" type="text"/>
-                  </div>
-                  <div className="form__row">
-                    <label className="label label-block">Password</label>
-                    <input className="input" type="password"/>
-                  </div>
-                </div>
-                <div className="layout__row layout layout-align_vertical layout-align_justify">
-                  <a href="#" className="link">Password reminder</a>
-                  <button className="button button-green">Login</button>
-                </div>
-              </div>
+    <div className="box box-middle">
+      <header className="box__title">Register</header>
+      <form action="" onSubmit={this.submitHandler}>
+        <div className="box__body">
+          <div className="layout__row">
+            <div className="form__row">
+              <label className="label label-block">First name</label>
+              <input className="input" type="text" name="username"/>
             </div>
-            <div className="box box-middle">
-              <header className="box__title">Register</header>
-              <form action="" onSubmit={this.submitHandler}>
-                <div className="box__body">
-                  <div className="layout__row">
-                    <div className="form__row">
-                      <label className="label label-block">First name</label>
-                      <input className="input" type="text" name="username"/>
-                    </div>
-                    <div className="form__row">
-                      <label className="label label-block">Last name</label>
-                      <input className="input" type="text"/>
-                    </div>
-                    <div className="form__row">
-                      <label className="label label-block">Email</label>
-                      <input className="input" type="email" name="email"/>
-                    </div>
-                    <div className="form__row">
-                      <label className="label label-block">Password</label>
-                      <input className="input" type="password" name="password"/>
-                    </div>
-                    <div className="form__row">
-                      <label className="label label-block">Repeat password</label>
-                      <input className="input" type="password" name="password_repeat"/>
-                    </div>
-                  </div>
-                  <div className="layout__row layout layout-align_vertical layout-align_justify">
-                    <button className="button button-yellow">Send</button>
-                  </div>
-                </div>
-              </form>
+            <div className="form__row">
+              <label className="label label-block">Last name</label>
+              <input className="input" type="text"/>
+            </div>
+            <div className="form__row">
+              <label className="label label-block">Email</label>
+              <input className="input" type="email" name="email"/>
+            </div>
+            <div className="form__row">
+              <label className="label label-block">Password</label>
+              <input className="input" type="password" name="password"/>
+            </div>
+            <div className="form__row">
+              <label className="label label-block">Repeat password</label>
+              <input className="input" type="password" name="password_repeat"/>
             </div>
           </div>
+          <div className="layout__row layout layout-align_vertical layout-align_justify">
+            <button className="button button-yellow">Send</button>
+          </div>
         </div>
-      </div>
+      </form>
+    </div>
     )
   }
 }
+
+
+function select(state) {
+  return state;
+}
+
+export default connect(select)(RegisterComponent);
