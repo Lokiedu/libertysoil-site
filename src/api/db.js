997403afbb253953fef26a43eeeb9558cea68638
Roman Getman <romangetman@gmail.com>
2015-09-09 19:46:23 +0300

    Update relationship mapping

--- src/api/db.js
+++ src/api/db.js
@@ -16,10 +16,10 @@ export default function initBookshelf(config) {
       return this.hasMany(Post, 'user_id');
     },
     following: function() {
-      return this.belongsToMany(User, 'followers', 'id', 'user_id');
+      return this.belongsToMany(User, 'followers', 'user_id', 'following_user_id');
     },
     followers: function() {
-      return this.belongsToMany(User, 'followers', 'id', 'following_user_id');
+      return this.belongsToMany(User, 'followers', 'following_user_id', 'user_id');
     },
     hidden: ['hashed_password', 'email']  // exclude from json-exports
   });
