e48c040e7fde11e76b26e3f25b2f7cf502d65997
Alexey Zakhlestin <indeyets@gmail.com>
2015-09-25 15:03:47 +0300

    single post page

--- src/routing.js
+++ src/routing.js
@@ -5,11 +5,13 @@ import React from 'react';
 import App from './pages/app'
 import Auth from './pages/auth'
 import MaybeList from './pages/maybe_list'
+import PostPage from './pages/post'
 
 let routes = (
   <Route component={App}>
     <Route component={MaybeList} name="post_list" path="/" />
     <Route component={Auth} name="auth" path="/auth" />
+    <Route component={PostPage} name="post" path="/post/:uuid" />
   </Route>
 );
 
