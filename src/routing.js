9bb6c32eccb46f4a5df508a2815f19edcc987047
Roman Getman <romangetman@gmail.com>
2015-09-25 19:04:04 +0300

    Add user page routing

--- src/routing.js
+++ src/routing.js
@@ -6,12 +6,14 @@ import App from './pages/app'
 import Auth from './pages/auth'
 import MaybeList from './pages/maybe_list'
 import PostPage from './pages/post'
+import UserPage from './pages/user'
 
 let routes = (
   <Route component={App}>
     <Route component={MaybeList} name="post_list" path="/" />
     <Route component={Auth} name="auth" path="/auth" />
     <Route component={PostPage} name="post" path="/post/:uuid" />
+    <Route component={UserPage} name="user" path="/user/:username" />
   </Route>
 );
 
