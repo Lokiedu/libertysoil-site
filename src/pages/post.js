662631186ed1d6e1a816659dcfee93348523f8b1
Alexey Zakhlestin <indeyets@gmail.com>
2015-09-25 15:51:30 +0300

    fetch post from store

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import request from 'superagent';
+import _ from 'lodash';
 
 import Header from '../components/header';
 import Footer from '../components/footer';
@@ -12,14 +13,26 @@ import {API_HOST} from '../config';
 import {getStore, setPosts} from '../store';
 
 
-class Index extends React.Component {
+class PostPage extends React.Component {
   async componentWillMount() {
     let result = await request.get(`${API_HOST}/api/v1/posts`);
     getStore().dispatch(setPosts(result.body));
   }
 
   render() {
-    let current_user = this.props.current_user;
+    // FIXME: add check for post existence
+    const current_user = this.props.current_user;
+
+    if (this.props.posts.length == 0) {
+      return <script/>  // still loading
+    }
+
+    const post_uuid = this.props.params.uuid;
+    const current_post = _.find(this.props.posts, {id: post_uuid});
+
+    if (!current_post) {
+      return <script/>  // 404
+    }
 
     return (
       <div>
@@ -30,6 +43,7 @@ class Index extends React.Component {
 
             <div className="page__content">
               <p>FIXME</p>
+              <p>text: "{current_post.text}"</p>
             </div>
           </div>
         </div>
@@ -43,4 +57,4 @@ function select(state) {
   return state.toJS();
 }
 
-export default connect(select)(Index);
+export default connect(select)(PostPage);
