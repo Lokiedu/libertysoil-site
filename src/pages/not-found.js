c409da403c4023f275d9551eb86b2015e3e00394
Alexey Zakhlestin <indeyets@gmail.com>
2015-10-16 00:25:13 +0300

    use selector for NotFound

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -17,14 +17,14 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
+import { defaultSelector } from '../selectors';
 
 class NotFound extends React.Component {
   render() {
-    let current_user = this.props.users[this.props.current_user];
-
     return (
       <div>
-        <Header is_logged_in={this.props.is_logged_in} current_user={current_user} />
+        <Header is_logged_in={this.props.is_logged_in} current_user={this.props.current_user} />
+
         <div className="page__container">
           <div className="page__body">
             <section className="card">
@@ -39,8 +39,4 @@ class NotFound extends React.Component {
   }
 }
 
-function select(state) {
-  return state.toJS();
-}
-
-export default connect(select)(NotFound);
+export default connect(defaultSelector)(NotFound);
