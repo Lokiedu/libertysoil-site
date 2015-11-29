16a087125e1ba15e27ba332be725256fb5233b13
Alexey Zakhlestin <indeyets@gmail.com>
2015-11-29 17:44:33 +0300

    sort schools by name + eslint fixes

--- src/components/post/school-select.js
+++ src/components/post/school-select.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class SchoolSelect extends Component {
+  static displayName = 'SchoolSelect';
   static propTypes = {
     schools: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string
@@ -11,18 +12,20 @@ export default class SchoolSelect extends Component {
     schools: []
   };
 
+  _renderOptions() {
+    let schools = this.props.schools.sort((a, b) => a.name.localeCompare(b.name));
+
+    return schools.map((school, i) => {
+      return <option key={i} value={school.name}>{school.name}</option>;
+    });
+  }
+
   render() {
     return (
-      <select className="input" name="school" defaultValue="" {...this.props}>
-        <option disabled value="">Select school...</option>
+      <select className="input" defaultValue="" name="school" {...this.props}>
+        <option disabled key="-1" value="">Select school...</option>
         {this._renderOptions()}
       </select>
     );
-  }
-
-  _renderOptions() {
-    return this.props.schools.map((school, i) => {
-      return <option value={school.name} key={i}>{school.name}</option>;
-    });
   }
 }
