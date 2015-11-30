ea7fb6fc63b37daf3e8af643bc91f79ad7362bf8
Dmitry Vdovin <voidxnull@gmail.com>
2015-11-30 17:44:01 +0200

    Fix migration 20151130141514_add_lat_and_long_to_schools

--- migrations/20151130141514_add_lat_and_long_to_schools.js
+++ migrations/20151130141514_add_lat_and_long_to_schools.js
@@ -1,12 +1,12 @@
 export async function up(knex, Promise) {
-  knex.schema.table('schools', table => {
+  await knex.schema.table('schools', table => {
     table.decimal('lat');
     table.decimal('long');
   });
 }
 
 export async function down(knex, Promise) {
-  knex.schema.table('schools', table => {
+  await knex.schema.table('schools', table => {
     table.dropColumn('lat');
     table.dropColumn('long');
   });
