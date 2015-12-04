8a78f303ede99a99c289b2610b12d788e3842782
Alexey Zakhlestin <indeyets@gmail.com>
2015-12-04 19:12:14 +0300

    Reset password feature:
    - migration
    - new React pages: "Reset Password" and "New Password"
    - initial controller actions

---
+++ src/pages/new-password.js
@@ -0,0 +1,71 @@
+/*
+ This file is a part of libertysoil.org website
+ Copyright (C) 2015  Loki Education (Social Enterprise)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { defaultSelector } from '../selectors';
+
+import Footer from '../components/footer';
+import Header from '../components/header';
+
+class Form extends React.Component {
+
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    let form = event.target;
+
+      /*     this.props.onLoginUser(form.username.value, form.password.value); */
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <form onSubmit={this.submitHandler} action="" method="post">
+          <div className="page__body">
+            <div className="area">
+              <div>
+                <div className="area__body layout-align_start">
+                <div className="box box-middle">
+                  <header className="box__title">Set new password</header>
+                    <div className="box__body">
+                      <div className="layout__row">
+                        <div className="form__row">
+                          <label className="label label-block label-space" htmlFor="newPassword">New Password</label>
+                          <input className="input input-block" id="newPassword" required="required" type="text" name="new_password"/>
+                          <label className="label label-block label-space" htmlFor="newPasswordRepeat">Repeat</label>
+                          <input className="input input-block" id="newPasswordRepeat" required="required" type="text" name="new_password_repeat"/>
+                        </div>
+                      </div>
+                      <div className="layout__row layout layout-align_vertical layout-align_justify">
+                        <button type="submit" className="button button-wide button-green">Submit</button>
+                      </div>
+                    </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+        <Footer/>
+      </div>
+      );
+  }
+}
+
+export default connect(defaultSelector)(Form);
