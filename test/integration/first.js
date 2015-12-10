7a08ccef0162c2613781edfb16fbcb8938fcadfa
Alexander Pletnev <pletnev.rusalex@gmail.com>
2015-12-10 15:22:42 +0300

    First test First test

--- test/integration/first.js
+++ test/integration/first.js
@@ -1,8 +1,30 @@
 import bb from 'bluebird';
+import uuid from 'uuid';
+import initBookshelf from '../../src/api/db';
+import db_config from '../../knexfile';
+let bookshelf = initBookshelf(db_config['test']);
 
 describe('promise Test', function() {
 
-  describe('new Promise', function() {
+  describe('User.save Promise', function() {
+
+    let User = bookshelf.model('User');
+    let obj = new User({
+      id: uuid.v4(),
+      username: 'testuser',
+      hashed_password: 'test',
+      email: 'test@example.com'
+    });
+
+    /**
+     * Next statement fails with message: Unexpected token obj. Seems like mocha do not understand async nature of
+     * bookshelf model.
+     */
+    // await obj.save(null, {method: 'insert'});
+
+  });
+
+  describe('Simple bluebird promise', function() {
 
     let f = () => {
       return new bb((resolve, reject) => {
