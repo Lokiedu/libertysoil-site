02abc78b24fda972de47c8cbe791a6f8a28d5d8f
Dmitry Vdovin <voidxnull@gmail.com>
2015-12-15 05:51:50 +0200

    Add test/expect.js

---
+++ test/expect.js
@@ -0,0 +1,23 @@
+import expect from 'unexpected';
+import app from '../index';
+
+expect.installPlugin(require('unexpected-express'));
+
+expect.addAssertion('to yield a response of', function (expect, subject, value) {
+  return expect(app, 'to yield exchange', {
+    request: subject,
+    response: value
+  });
+});
+
+// TODO: Expect to yield a redirect to a specific path.
+expect.addAssertion('to redirect', function (expect, subject, value) {
+  return expect(app, 'to yield exchange', {
+    request: subject,
+    response: {
+      statusCode: 301
+    }
+  });
+});
+
+export default expect;
