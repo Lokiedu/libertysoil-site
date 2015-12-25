a19cf6c41be9051a3cbf91381e8fa99198f925d0
Alexey Zakhlestin <indeyets@gmail.com>
2015-12-25 16:30:09 +0300

    added license info

--- tasks.js
+++ tasks.js
@@ -1,7 +1,26 @@
+/*
+ This file is a part of libertysoil.org website
+ Copyright (C) 2015  Loki Education (Social Enterprise)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import kueLib from 'kue';
+
 import config from './config';
 import { renderWelcomeTemplate, renderResetTemplate } from './src/email-templates/index';
 import { sendEmail } from './src/utils/email';
+
 
 let queue = kueLib.createQueue(config.kue);
 
