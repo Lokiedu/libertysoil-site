0a2a92cc84829e3b96062a6ce6fc52beb47d1495
Alexander Pletnev <pletnev.rusalex@gmail.com>
2015-12-25 11:57:16 +0300

    Reset password email task.

--- tasks.js
+++ tasks.js
@@ -1,9 +1,18 @@
 import kueLib from 'kue';
 import config from './config';
+import { renderWelcomeTemplate, renderResetTemplate } from './src/email-templates/index';
+import { sendEmail } from './src/utils/email';
 
 let queue = kueLib.createQueue(config.kue);
 
-queue.process('reset-password-email', function(job, done){
-  console.log(job.data);
+queue.process('reset-password-email', async function(job, done) {
+  let html = await renderResetTemplate(new Date(), job.data.username, job.data.email, `http://www.libertysoil.org/newpassword/${job.data.hash}`);
+  await sendEmail('Reset Libertysoil.org Password', html, job.data.email);
   done();
 });
+
+// queue.process('reset-password-email', async function(job, done){
+//   let html = await renderResetTemplate(new Date(), job.data.username, job.data.email, `http://www.libertysoil.org/newpassword/${job.data.hash}`);
+//   await sendEmail('Reset Libertysoil.org Password', html, job.data.email);
+//   done();
+// });
