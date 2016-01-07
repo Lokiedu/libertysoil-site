2ecd2e8944b910f5a244f35ab783a6d6fb1c5e33
Dmitry Vdovin <voidxnull@gmail.com>
2016-01-07 06:28:50 +0200

    Refactor CreatePost

--- src/components/create-post.js
+++ src/components/create-post.js
@@ -15,7 +15,7 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import React from 'react';
+import React, {PropTypes} from 'react';
 import _ from 'lodash';
 
 import postTypeConstants from '../consts/postTypeConstants';
@@ -26,28 +26,36 @@ import MoreButton from './more-button';
 import * as TagType from '../utils/tags';
 import ClickOutsideComponentDecorator from '../decorators/ClickOutsideComponentDecorator';
 
-import AddTagModal from '../components/add-tag-modal';
+import AddHashtagModal from './add-hashtag-modal';
 
 
 @ClickOutsideComponentDecorator
 export default class CreatePost extends React.Component {
   static displayName = 'CreatePost';
   static propTypes = {
-    actions: React.PropTypes.shape({
-      resetCreatePostForm: React.PropTypes.func,
-      updateCreatePostForm: React.PropTypes.func
+    actions: PropTypes.shape({
+      resetCreatePostForm: PropTypes.func,
+      updateCreatePostForm: PropTypes.func
     }),
-    defaultText: React.PropTypes.string,
-    tags: React.PropTypes.arrayOf(React.PropTypes.string),
-    triggers: React.PropTypes.shape({
-      createPost: React.PropTypes.func.isRequired
+    defaultText: PropTypes.string,
+    locations: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string
+    })),
+    schools: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string
+    })),
+    tags: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string
+    })),
+    triggers: PropTypes.shape({
+      createPost: PropTypes.func.isRequired
     })
   };
 
   state = {
     expanded: false,
-    isAddTagModalVisible: false
-  }
+    addTagModalVisible: false
+  };
 
   onClickOutside = () => {
     let form = this.refs.form;
@@ -70,7 +78,7 @@ export default class CreatePost extends React.Component {
 
     await this.props.triggers.createPost('short_text', {
       text: form.text.value,
-      tags: this.props.tags
+      tags: this.props.tags.map(tag => tag.name)
     });
 
     form.text.value = '';
@@ -88,105 +96,103 @@ export default class CreatePost extends React.Component {
     });
   };
 
-  _showAddTagModal = () => {
+  _showAddHashtagModal = () => {
     this.setState({
-      isAddTagModalVisible: true
+      addTagModalVisible: true,
+      addTagModalType: TagType.TAG_HASHTAG
+    });
+  };
+
+  _showAddSchoolModal = () => {
+    this.setState({
+      addTagModalVisible: true,
+      addTagModalType: TagType.TAG_SCHOOL
+    });
+  };
+
+  _showAddLocationModal = () => {
+    this.setState({
+      addTagModalVisible: true,
+      addTagModalType: TagType.TAG_LOCATION
     });
   };
 
   _closeAddTagModal = () => {
     this.setState({
-      isAddTagModalVisible: false
-    });
-  };
-
-  _addTags = (tags) => {
-    this.props.actions.updateCreatePostForm({tags});
-
-    this.setState({
-      isAddTagModalVisible: false
+      addTagModalVisible: false
     });
   };
 
   /**
-   * Renders a textarea with an artificial caret.
-   * @private
+   * Adds the tags to the redux form state.
+   * Used in the tag modal.
+   * @param {Object} tags - {tags: [], schools: [], locations: [], ...}
    */
-  _renderTextarea() {
-    return (
-      <div className="create_post__text_input_wrapper">
-        {(!this.state.expanded) ? <div className="create_post__caret"></div> : null}
-        <textarea
-          className="input input-block create_post__text_input"
-          defaultValue={this.props.defaultText}
-          name="text"
-          placeholder="Make a contribution to education change"
-          rows={(this.state.expanded) ? 10 : 1}
-          onFocus={this._handleFocus}
-        />
-      </div>
-    );
-  }
+  _addTags = (tags) => {
+    this.props.actions.updateCreatePostForm(tags);
 
-  _renderTagButtons() {
-    if (this.state.expanded) {
-      return (
-        <div className="layout layout-rows layout-align_vertical">
-          <TagIcon className="create_post__tag_button" type={TagType.TAG_SCHOOL} />
-          <TagIcon className="create_post__tag_button" type={TagType.TAG_LOCATION} />
-          {/*<TagIcon className="create_post__tag_button" type={TagType.TAG_EVENT} />*/}
-          {/*<TagIcon className="create_post__tag_button" type={TagType.TAG_MENTION} />*/}
-          <TagIcon className="create_post__tag_button" type={TagType.TAG_HASHTAG} onClick={this._showAddTagModal} />
-        </div>
-      );
-    }
-  }
-
-  _renderFooter() {
-    if (this.state.expanded) {
-      return (
-        <div className="layout__row layout layout-align_vertical">
-          <div className="layout__grid_item">
-            <button className="button button-wide button-red" type="submit">Publish</button>
-          </div>
-          <div className="layout__grid_item">
-            <button className="button button-wide button-transparent" type="button">Go full screen</button>
-          </div>
-        </div>
-      );
-    }
-  }
-
-  _renderAddtagModal() {
-    if (this.state.isAddTagModalVisible) {
-      return (
-        <AddTagModal
-          tags={this.props.tags}
-          onClose={this._closeAddTagModal}
-          onSave={this._addTags}
-        />
-      );
-    }
-  }
+    this.setState({
+      addTagModalVisible: false
+    });
+  };
 
   render () {
+    let { addTagModalVisible, expanded } = this.state;
+
     return (
       <div className="box box-post box-space_bottom create_post">
         <form ref="form" onSubmit={this._handleSubmit}>
           <div className="box__body">
             <div className="layout__row layout layout-columns layout-align_start">
               <div className="layout__grid_item layout__grid_item-wide">
-                {this._renderTextarea()}
-                {this._renderFooter()}
+                <div className="create_post__text_input_wrapper">
+                  {!expanded &&
+                    <div className="create_post__caret"></div>
+                  }
+                  <textarea
+                    className="input input-block create_post__text_input"
+                    defaultValue={this.props.defaultText}
+                    name="text"
+                    placeholder="Make a contribution to education change"
+                    rows={(this.state.expanded) ? 10 : 1}
+                    onFocus={this._handleFocus}
+                  />
+                </div>
+                {expanded &&
+                  <div className="layout__row layout layout-align_vertical">
+                    <div className="layout__grid_item">
+                      <button className="button button-wide button-red" type="submit">Publish</button>
+                    </div>
+                    <div className="layout__grid_item">
+                      <button className="button button-wide button-transparent" type="button">Go full screen</button>
+                    </div>
+                  </div>
+                }
               </div>
               <div className="layout__grid_item layout__grid_item-small layout layout-rows layout-align_vertical">
-                <MoreButton expanded={this.state.expanded} onClick={this._handleClickOnMore} />
-                {this._renderTagButtons()}
+                <MoreButton expanded={expanded} onClick={this._handleClickOnMore} />
+                {expanded &&
+                  <div className="layout layout-rows layout-align_vertical">
+                    <TagIcon className="create_post__tag_button" type={TagType.TAG_SCHOOL} onClick={this._showAddSchoolModal} />
+                    <TagIcon className="create_post__tag_button" type={TagType.TAG_LOCATION} onClick={this._showAddLocationModal}  />
+                    {/*<TagIcon className="create_post__tag_button" type={TagType.TAG_EVENT} />*/}
+                    {/*<TagIcon className="create_post__tag_button" type={TagType.TAG_MENTION} />*/}
+                    <TagIcon className="create_post__tag_button" type={TagType.TAG_HASHTAG} onClick={this._showAddHashtagModal} />
+                  </div>
+                }
               </div>
             </div>
           </div>
         </form>
-        {this._renderAddtagModal()}
+        {addTagModalVisible &&
+          <AddHashtagModal
+            locations={this.props.locations}
+            schools={this.props.schools}
+            tags={this.props.tags}
+            onClose={this._closeAddTagModal}
+            onSave={this._addTags}
+          />
+        }
       </div>
     )
   }
