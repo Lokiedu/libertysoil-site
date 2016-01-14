0dfb0f180cc146929207bb0c46d88e0bac449739
Roman <w2@live.ru>
2016-01-14 11:40:51 +0200

    user change avatar updated

--- src/components/settings/change-avatar.js
+++ src/components/settings/change-avatar.js
@@ -16,8 +16,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import React, { PropTypes } from 'react';
-import Modal from 'react-modal';
-import AvatarEditor from 'react-avatar-editor';
+
+import ChangeAvatarModal from './change-avatar-modal';
 
 class ChangeAvatar extends React.Component {
   static displayName = 'ChangeAvatar';
@@ -29,60 +29,24 @@ class ChangeAvatar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	modalIsOpen: false,
       image: null,
       scale: 1,
-      avatar: null
+      avatar: null,
+      modalVisible: false
     };
   }
 
   openModal = () => {
-    this.setState({modalIsOpen: true});
+    this.setState({modalVisible: true});
   }
 
   closeModal = () => {
-    this.setState({modalIsOpen: false, image: null});
+    this.setState({modalVisible: false});
   }
 
-  onChange = (event) => {
-    let file = event.target.files[0];
-    this.setState({
-      image: URL.createObjectURL(file),
-      avatar: file
-    });
-  }
-
-  handleScale = () => {
-    this.setState({scale: this.refs.scale.value})
-  }
-
-  submit = () => {
-    let crop = this.refs.avatar.getCroppingRect();
-    this.props.onChange(this.state.avatar, crop);
+  submit = (image, crop) => {
+    this.props.onChange(image, crop);
     this.closeModal();
-  }
-
-  editor() {
-    return this.state.image ? (
-            <div>
-              <AvatarEditor
-              ref="avatar"
-              image={this.state.image}
-              width={350}
-              height={350}
-              border={50}
-              color={[255, 255, 255, 0.6]} // RGBA
-              scale={this.state.scale} />
-              <div className="user_box__avatar_modal__size_box">
-                <label className="user_box__avatar_modal__size_box__icon">-</label>
-                <input className="user_box__avatar_modal__size_box__bar" name="scale" type="range" ref="scale" onChange={this.handleScale} min="1" max="2" step="0.01" defaultValue="1" />
-                <label className="user_box__avatar_modal__size_box__icon">+</label>
-              </div>
-              <div>
-                <button className="button button-green action" onClick={this.submit}>Update Avatar</button>
-              </div>
-            </div>
-          ) : (<input type="file" onChange={this.onChange} />);
   }
 
   render() {
@@ -91,33 +55,11 @@ class ChangeAvatar extends React.Component {
         <button onClick={this.openModal}>
           <span className="micon">camera</span>
         </button>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}
-          style={
-            {
-              content: {
-                left: '0',
-                right: '0',
-                width: '90%',
-                margin: '0 auto',
-                maxWidth: '600px',
-                textAlign: 'center'
-              }
-            }
-          }
-          >
-          <div className="user_box__avatar_modal modal">
-
-          <h2 className="content__sub_title layout__row">Change your avatar</h2>
-          <button className="modal__close" onClick={this.closeModal}>
-            <span className="micon">close</span>
-          </button>
-          <div>
-            {this.editor()}
-          </div>
-          </div>
-        </Modal>
+        <ChangeAvatarModal
+          visible={this.state.modalVisible}
+          onClose={this.closeModal}
+          onSave={this.submit}
+        />
       </div>
     );
   }
