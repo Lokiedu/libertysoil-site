fdf6befb708109355c65e567e11b016d934ea9c7
Alexey Zakhlestin <indeyets@gmail.com>
2016-01-15 11:19:01 +0300

    Fixed export of const

--- src/consts/profileConstants.js
+++ src/consts/profileConstants.js
@@ -1,14 +1,10 @@
-const ROLES = [
+export const ROLES = [
     'Teacher or School Staff Member',
     'Parent',
     'School Student',
     'School Director',
     'Journalist / Media'
 ];
-
-export default {
-    ROLES
-};
 
 export const AVATAR_SIZE = {
   width: 168,
