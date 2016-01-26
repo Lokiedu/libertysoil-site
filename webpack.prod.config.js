d00d9fd98e784da40cddd4b09ceb1870041d4c47
Dmitry Vdovin <voidxnull@gmail.com>
2016-01-27 00:24:00 +0200

    Add webpack.prod.config.js

---
+++ webpack.prod.config.js
@@ -0,0 +1,74 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/scripts/app.js',
+    './src/less/styles.less'
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.less']
+  },
+
+  output: {
+    path: path.join(__dirname, '/public/assets'),
+    filename: 'app.js',
+    publicPath: `/assets/`
+  },
+
+  module: {
+    postLoaders: [
+      // Fix grapheme-breaker
+      {
+        test: /\.js$/,
+        include: /node_modules\/grapheme-breaker/,
+        loader: 'transform/cacheable?brfs'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'transform?envify'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: [
+            'es2015-webpack',
+            'react',
+            'stage-0'
+          ],
+          plugins: [
+            ['transform-decorators-legacy']
+          ]
+        }
+      },
+      {test: /\.json$/, loader: 'json'},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css!postcss')},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less')},
+      {test: /\.(ttf|eot|woff(2)?)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=15000'},
+      {test: /\.(png|jpg|svg)$/, loader: 'url?limit=15000'}
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true
+    })
+  ],
+
+  postcss: function () {
+    return [autoprefixer];
+  }
+};
