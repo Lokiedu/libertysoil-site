2dd1ff8899175ad627ba78239f30e459d1becf27
Alexander Pletnev <pletnev.rusalex@gmail.com>
2016-01-30 00:02:52 +0000

    ResetPassword page tests: - it must render <ResetForm /> by default - it must contain <SuccessMessage /> when ui.submitResetPassword is true

---
+++ test/unit/pages/password-reset.js
@@ -0,0 +1,40 @@
+/*
+ This file is a part of libertysoil.org website
+ Copyright (C) 2015  Loki Education (Social Enterprise)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*eslint-env node, mocha */
+import { TestUtils, expect, React } from '../../../test-helpers/expect-unit';
+
+import { Form as PasswordResetPage, ResetForm, SuccessMessage } from '../../../src/pages/password-reset';
+
+
+describe('PasswordResetPage', function() {
+
+  it('MUST renders <ResetForm /> by default', function() {
+    let renderer = TestUtils.createRenderer();
+    renderer.render(<PasswordResetPage ui={{submitResetPassword: undefined}} />);
+
+    return expect(renderer, 'to contain', <ResetForm />);
+  });
+
+  it('MUST contain <SuccessMessage /> when ui.submitResetPassword is true', function() {
+    let renderer = TestUtils.createRenderer();
+    renderer.render(<PasswordResetPage ui={{submitResetPassword: true}} />);
+
+    return expect(renderer, 'to contain', <SuccessMessage />);
+  });
+
+});
