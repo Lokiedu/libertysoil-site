524828de69042e46bd64b26e2412667a8a1b564e
Dmitry Vdovin <voidxnull@gmail.com>
2016-02-05 04:44:18 +0200

    Update updated_at when post is updated

--- src/api/controller.js
+++ src/api/controller.js
@@ -1108,6 +1108,9 @@ export default class ApiController {
       }
     }
 
+    // toJSON is important. It translates the date to UTC.
+    post_object.attributes.updated_at = new Date().toJSON();
+
     try {
       await post_object.save(null, {method: 'update'});
 
