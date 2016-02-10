eeb407e79f396903b8b52ac21f77363f47e5bc80
Aleksej Romanovskij <aleksij@gmail.com>
2016-02-10 17:42:18 +0200

    Breadcrumbs styles

--- src/components/tag.js
+++ src/components/tag.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 import { truncate } from 'grapheme-utils';
 import TagIcon from './tag-icon';
-import { TAG_HASHTAG, TAG_SCHOOL, TAG_LOCATION } from '../consts/tags';
+import { TAG_HASHTAG, TAG_SCHOOL, TAG_LOCATION, SIZE } from '../consts/tags';
 
 
 export default class Tag extends React.Component {
@@ -13,7 +13,9 @@ export default class Tag extends React.Component {
   static propTypes = {
     deletable: PropTypes.bool,
     truncated: PropTypes.bool,
+    inactive: PropTypes.bool,
     name: PropTypes.string,
+    size: PropTypes.string,
     type: PropTypes.oneOf([TAG_HASHTAG, TAG_SCHOOL, TAG_LOCATION]),
     urlId: PropTypes.string
   };
@@ -30,11 +32,12 @@ export default class Tag extends React.Component {
   };
 
   render() {
-    let { urlId, name, type, truncated } = this.props;
+    let { urlId, name, type, truncated, size, inactive, collapsed, ...props } = this.props;
     let tagName = name;
     let tagNameComponent;
     let tagIcon = <TagIcon className="tag__icon" type={type} />;
     let className;
+    let title = name;
     let url;
 
     switch (type) {
@@ -57,11 +60,28 @@ export default class Tag extends React.Component {
       }
     }
 
+    switch (size) {
+      case SIZE.BIG: {
+        className += ' tag-size_big';
+        break;
+      }
+    }
+
+    if (inactive) {
+      className += ' tag-inactive';
+    }
+
     if (truncated) {
       tagName = truncate(name, {length: 16});
     }
 
-    tagNameComponent = <div className="tag__name">{tagName}</div>;
+    if (!collapsed && tagName) {
+      tagNameComponent = <div className="tag__name">{tagName}</div>;
+    }
+
+    if (!title) {
+      title = this.props.title;
+    }
 
     // FIXME: this should be reimplemented as 2 different components
     if (this.props.deletable) {
@@ -76,7 +96,7 @@ export default class Tag extends React.Component {
       );
     } else {  // eslint-disable-line no-else-return
       return (
-        <Link className={`tag ${className}`} title={name} to={url}>
+        <Link className={`tag ${className}`} title={title} to={url}>
           <div className="tag__icon_wrapper">
             {tagIcon}
           </div>
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -24,9 +24,79 @@ import WhatIsLibertySoil from '../components/WhatIsLibertySoil';
 import Reviews from '../components/Reviews';
 import Footer from '../components/footer';
 
+import Tag from '../components/tag';
+
 let Welcome = () => {
   return (
     <div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item breadcrumbs__title">News Feed</div>
+          </div>
+        </div>
+      </div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo logo-size_small" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item"><Tag type="TAG_HASHTAG" size="BIG" /></div>
+            <div className="breadcrumbs__item breadcrumbs__title">All Hashtags</div>
+          </div>
+        </div>
+      </div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo logo-size_small" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item"><Tag type="TAG_HASHTAG" inactive={true} /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_HASHTAG" name="AERO 2016" /></div>
+          </div>
+        </div>
+      </div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo logo-size_small" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" size="BIG" /></div>
+            <div className="breadcrumbs__item breadcrumbs__title">All Locations</div>
+          </div>
+        </div>
+      </div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo logo-size_small" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" inactive={true} collapsed={true} name="All Locations" /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" name="North America" /></div>
+          </div>
+        </div>
+      </div>
+      <div className="header page__header">
+        <div className="header__body">
+          <div className="header__logo">
+            <a className="logo logo-size_small" title="Liberty Soil" href="/"><span className="logo__title">Liberty Soil</span></a>
+          </div>
+          <div className="header__breadcrumbs breadcrumbs">
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" inactive={true} collapsed={true} name="All Locations" /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" inactive={true} collapsed={true} name="North America" /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" inactive={true} name="USA" /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" inactive={true} name="Massachusetts" /></div>
+            <div className="breadcrumbs__item"><Tag type="TAG_LOCATION" name="Boston" /></div>
+          </div>
+        </div>
+      </div>
       <Helmet title="Welcome to " />
       <div className="page__container-bg font-open_sans font-light">
         <section className="landing landing-big landing-bg">
@@ -45,106 +115,106 @@ let Welcome = () => {
             </p>
             <p>We encourage people to collect, share and discuss resources on teaching and learning, helping to build a better understanding of the diversity within education. We aim to help families and schools solve problems like bullying, academic pressure, depression, anxiety, professional and social isolation, to name just a few. Needless to say, poor early life experiences negatively affect each person and a society as a whole.</p>
           </div>
-        </div>
-        <section className="landing">
-          <header className="landing__header width">
-            <h3 className="landing__stats"><Link to="/">LibertySoil.org</Link> is in <i>alpha</i> stage starting <time dateTime="2015-10-05">October 5th, 2015</time>. Target public <i>beta</i> release date is October 28th, 2015. All LibertySoil.org code is released under the open source <a href="https://github.com/Lokiedu/libertysoil-site/…/master/LICENSE">AGPL license</a> and is free to use. Contribute on <a href="https://github.com/Lokiedu/libertysoil-site"><i>GitHub.</i></a></h3>
-          </header>
-        </section>
+          <section className="landing">
+            <header className="landing__header width">
+              <h3 className="landing__stats"><Link to="/">LibertySoil.org</Link> is in <i>alpha</i> stage starting <time dateTime="2015-10-05">October 5th, 2015</time>. Target public <i>beta</i> release date is October 28th, 2015. All LibertySoil.org code is released under the open source <a href="https://github.com/Lokiedu/libertysoil-site/…/master/LICENSE">AGPL license</a> and is free to use. Contribute on <a href="https://github.com/Lokiedu/libertysoil-site"><i>GitHub.</i></a></h3>
+            </header>
+          </section>
 
 
-      {/*  <div className="page__body">
-          <div className="page__content">
-            <div className="layout__space">
-              <h1 className="page__title">Popular Posts</h1>
-              <div className="layout__row">
-                <section className="post_card layout__row layout__row-double">
-                  <header className="post_card__title"><a href="#">Some notes on famous experiment</a></header>
-                  <div className="post_card__author">
-                    <section className="user_box">
-                      <div className="user_box__body">
-                        <p className="user_box__name">User Name</p>
-                      </div>
-                    </section>
-                  </div>
-                  <div className="post_card__footer layout">
-                    <span className="layout__grid_item">3 <span className="icon fa fa-heart-o"></span></span>
-                    <span className="layout__grid_item">12 <span className="icon fa fa-star-o"></span></span>
-                    <span className="layout__grid_item">3 <span className="icon fa fa-comment-o"></span></span>
-                  </div>
-                </section>
-                <section className="post_card layout__row layout__row-double">
-                  <header className="post_card__title"><a href="#">Some notes on famous experiment</a></header>
-                  <div className="post_card__author">
-                    <section className="user_box">
-                      <div className="user_box__body">
-                        <p className="user_box__name">User Name</p>
-                      </div>
-                    </section>
-                  </div>
-                  <div className="post_card__footer layout">
-                    <span className="layout__grid_item">3 <span className="icon fa fa-heart-o"></span></span>
-                    <span className="layout__grid_item">12 <span className="icon fa fa-star-o"></span></span>
-                    <span className="layout__grid_item">3 <span className="icon fa fa-comment-o"></span></span>
-                  </div>
-                </section>
+        {/*  <div className="page__body">
+            <div className="page__content">
+              <div className="layout__space">
+                <h1 className="page__title">Popular Posts</h1>
+                <div className="layout__row">
+                  <section className="post_card layout__row layout__row-double">
+                    <header className="post_card__title"><a href="#">Some notes on famous experiment</a></header>
+                    <div className="post_card__author">
+                      <section className="user_box">
+                        <div className="user_box__body">
+                          <p className="user_box__name">User Name</p>
+                        </div>
+                      </section>
+                    </div>
+                    <div className="post_card__footer layout">
+                      <span className="layout__grid_item">3 <span className="icon fa fa-heart-o"></span></span>
+                      <span className="layout__grid_item">12 <span className="icon fa fa-star-o"></span></span>
+                      <span className="layout__grid_item">3 <span className="icon fa fa-comment-o"></span></span>
+                    </div>
+                  </section>
+                  <section className="post_card layout__row layout__row-double">
+                    <header className="post_card__title"><a href="#">Some notes on famous experiment</a></header>
+                    <div className="post_card__author">
+                      <section className="user_box">
+                        <div className="user_box__body">
+                          <p className="user_box__name">User Name</p>
+                        </div>
+                      </section>
+                    </div>
+                    <div className="post_card__footer layout">
+                      <span className="layout__grid_item">3 <span className="icon fa fa-heart-o"></span></span>
+                      <span className="layout__grid_item">12 <span className="icon fa fa-star-o"></span></span>
+                      <span className="layout__grid_item">3 <span className="icon fa fa-comment-o"></span></span>
+                    </div>
+                  </section>
+                </div>
               </div>
             </div>
-          </div>
-          <aside className="page__sidebar">
-            <div className="layout__space">
-              <div className="layout__row layout__row-double">
-                <div className="group layout__row">
+            <aside className="page__sidebar">
+              <div className="layout__space">
+                <div className="layout__row layout__row-double">
+                  <div className="group layout__row">
+                    <div className="layout__row">
+                      <h3 className="head">Most followed people</h3>
+                    </div>
+                    <section className="layout__row user_box">
+                      <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
+                      <div className="user_box__body">
+                        <p className="user_box__name">User Name</p>
+                        <p className="user_box__text">Math teacher</p>
+                      </div>
+                    </section>
+                    <section className="layout__row user_box">
+                      <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
+                      <div className="user_box__body">
+                        <p className="user_box__name">User Name</p>
+                        <p className="user_box__text">Math teacher</p>
+                      </div>
+                    </section>
+                    <section className="layout__row user_box">
+                      <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
+                      <div className="user_box__body">
+                        <p className="user_box__name">User Name</p>
+                        <p className="user_box__text">Math teacher</p>
+                      </div>
+                    </section>
+                    <section className="layout__row user_box">
+                      <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
+                      <div className="user_box__body">
+                        <p className="user_box__name">User Name</p>
+                        <p className="user_box__text">Math teacher</p>
+                      </div>
+                    </section>
+                  </div>
+                </div>
+                <div className="group layout__row layout__row-double">
                   <div className="layout__row">
-                    <h3 className="head">Most followed people</h3>
+                    <h3 className="head">Popular tags</h3>
                   </div>
-                  <section className="layout__row user_box">
-                    <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
-                    <div className="user_box__body">
-                      <p className="user_box__name">User Name</p>
-                      <p className="user_box__text">Math teacher</p>
+                  <div className="layout__row">
+                    <div className="tags">
+                      <span className="tag">Psychology</span>
+                      <span className="tag">Gaming</span>
                     </div>
-                  </section>
-                  <section className="layout__row user_box">
-                    <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
-                    <div className="user_box__body">
-                      <p className="user_box__name">User Name</p>
-                      <p className="user_box__text">Math teacher</p>
-                    </div>
-                  </section>
-                  <section className="layout__row user_box">
-                    <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
-                    <div className="user_box__body">
-                      <p className="user_box__name">User Name</p>
-                      <p className="user_box__text">Math teacher</p>
-                    </div>
-                  </section>
-                  <section className="layout__row user_box">
-                    <img className="user_box__avatar" src="http://placehold.it/32x32" alt=""/>
-                    <div className="user_box__body">
-                      <p className="user_box__name">User Name</p>
-                      <p className="user_box__text">Math teacher</p>
-                    </div>
-                  </section>
-                </div>
-              </div>
-              <div className="group layout__row layout__row-double">
-                <div className="layout__row">
-                  <h3 className="head">Popular tags</h3>
-                </div>
-                <div className="layout__row">
-                  <div className="tags">
-                    <span className="tag">Psychology</span>
-                    <span className="tag">Gaming</span>
                   </div>
                 </div>
               </div>
-            </div>
-          </aside>
-        </div>*/}
+            </aside>
+          </div>*/}
+        </div>
+        <Reviews/>
+        <Footer/>
       </div>
-      <Reviews/>
-      <Footer/>
     </div>
   );
 };
