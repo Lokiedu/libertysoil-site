b8c011f16cac978d660ecc75acb7ca6d7a4e0e35
Dmitry Vdovin <voidxnull@gmail.com>
2016-02-10 17:58:09 +0200

    Add related_posts to redux

--- src/store/posts.js
+++ src/store/posts.js
@@ -43,6 +43,7 @@ export default function reducer(state=initialState, action) {
     case a.SET_TAG_POSTS:
     case a.SET_SCHOOL_POSTS:
     case a.SET_GEOTAG_POSTS:
+    case a.SET_RELATED_POSTS:
     {
       let postsWithoutUsers = _.indexBy(action.posts.map(post => {
         let postCopy = _.cloneDeep(post);
