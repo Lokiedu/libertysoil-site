20a91bcc8a04fc1cb6874e76666c6b0c959d98c8
Dmitry Vdovin <voidxnull@gmail.com>
2016-02-18 06:00:22 +0200

    Add basic factories for models: Geotag, Label, Post, School, User
    Usage:
      new Geotag(GeotagFactory.build()).save(null, {method: 'insert'})
    
    In future, we might want to simplify the process of creating a record
    from a factory.

---
+++ test-helpers/factories/user.js
@@ -0,0 +1,12 @@
+import uuid from 'uuid';
+import bcrypt from 'bcrypt';
+import { Factory } from 'rosie';
+const faker = require('faker');
+
+
+export default new Factory()
+  .attr('id', () => uuid.v4())
+  .attr('username', () => faker.internet.userName())
+  .attr('email', () => faker.internet.email())
+  .attr('password', () => faker.internet.password())
+  .attr('hashed_password', ['password'], password => bcrypt.hashSync(password, 10));
