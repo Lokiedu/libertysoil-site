82b2931b8c3d570de53b2270455174d8916e7058
Alexander Pletnev <pletnev.rusalex@gmail.com>
2016-02-26 05:10:57 +0000

    refactor queue unit test to prove actual created jobs using singleton

--- test/unit/utils/queue.js
+++ test/unit/utils/queue.js
@@ -4,16 +4,34 @@ import QueueSingleton from '../../../src/utils/queue';
 
 
 describe('Queue test', function() {
+  let queue;
 
-  beforeEach( () => {
-    this.queue = new QueueSingleton;
-    this.queue.setHandler('ccc'); // just a dummy value to test singleton
+  before(() => {
+    queue = new QueueSingleton().handler;
+    queue.testMode.enter();
   });
 
   it('Singleton should work', function() {
     let c = new QueueSingleton;
 
-    return expect(c.handler, 'to equal', 'ccc');
+    // prove global scope queue has no jobs
+    expect(queue.testMode.jobs.length, 'to equal', 0);
+
+    // create new job using local scope queue
+    c.handler.createJob('myJob', { foo: 'bar' }).save();
+
+    // prove global scope queue now has new job
+    expect(queue.testMode.jobs.length, 'to equal', 1);
+    expect(queue.testMode.jobs[0].type, 'to equal', 'myJob');
+    return expect(queue.testMode.jobs[0].data, 'to equal', { foo: 'bar' });
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit();
   });
 
 });
