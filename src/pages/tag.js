11f0de78a95024c63f0609aacd9c4f182fe5ae0f
Dmitry Vdovin <voidxnull@gmail.com>
2016-03-08 22:45:11 +0200

    Add breadcrumbs to TagPage

--- src/pages/tag.js
+++ src/pages/tag.js
@@ -17,20 +17,27 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
 import ApiClient from '../api/client'
 import { API_HOST } from '../config';
 import { setTagPosts } from '../actions';
+import Breadcrumbs from '../components/breadcrumbs';
 import Header from '../components/header';
+import HeaderLogo from '../components/header-logo';
 import Footer from '../components/footer';
 import River from '../components/river_of_posts';
 import Sidebar from '../components/sidebar';
 import SidebarAlt from '../components/sidebarAlt';
+import Tag from '../components/tag';
+import TagIcon from '../components/tag-icon';
 import FollowTagButton from '../components/follow-tag-button';
 import LikeTagButton from '../components/like-tag-button';
 import { ActionsTrigger } from '../triggers';
 import { defaultSelector } from '../selectors';
+import { TAG_HASHTAG } from '../consts/tags';
+
 
 export class TagPage extends Component {
   static displayName = 'TagPage';
@@ -62,7 +69,17 @@ export class TagPage extends Component {
     return (
       <div>
         <Helmet title={`"${this.props.params.tag}" posts on `} />
-        <Header is_logged_in={is_logged_in} current_user={current_user} />
+        <Header is_logged_in={is_logged_in} current_user={current_user}>
+          <HeaderLogo small />
+          <div className="header__breadcrumbs">
+            <Breadcrumbs>
+              <Link to="/tag" title="All Hashtags">
+                <TagIcon inactive type={TAG_HASHTAG} />
+              </Link>
+              <Tag name={this.props.params.tag} type={TAG_HASHTAG} urlId={this.props.params.tag} />
+            </Breadcrumbs>
+          </div>
+        </Header>
 
         <div className="page__container">
           <div className="page__body">
