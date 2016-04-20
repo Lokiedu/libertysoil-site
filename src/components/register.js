3800bd31aeb2fefa9a30b5555f06876a0e8d53c3
Oopscurity <headlessfirst@gmail.com>
2016-04-20 09:11:24 +0300

    New Register form's validation

--- src/components/register.js
+++ src/components/register.js
@@ -15,11 +15,13 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { form as inform } from 'react-inform';
 import ga from 'react-google-analytics';
 
 import ApiClient from '../api/client';
 import { API_HOST } from '../config';
+import Message from './message';
 
 class SuccessContent extends Component {
 
@@ -43,68 +45,70 @@ class SuccessContent extends Component {
   }
 }
 
-export default class RegisterComponent extends React.Component {
+class Register extends React.Component {
+  static displayName = 'Register';
+
+  static propTypes = {
+    fields: PropTypes.shape({
+      username: PropTypes.shape({
+        error: PropTypes.string
+      }).isRequired,
+      password: PropTypes.shape({
+        error: PropTypes.string
+      }).isRequired,
+      passwordRepeat: PropTypes.shape({
+        error: PropTypes.string
+      }).isRequired,
+      email: PropTypes.shape({
+        error: PropTypes.string
+      }).isRequired,
+      agree: PropTypes.shape({
+        error: PropTypes.string
+      }).isRequired
+    }).isRequired,
+    form: PropTypes.shape({
+      forceValidate: PropTypes.func.isRequired,
+      isValid: PropTypes.func.isRequired,
+      onValues: PropTypes.func.isRequired
+    }).isRequired
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    const { fields, form } = this.props;
+
+    form.forceValidate();
+
+    if (!form.isValid()) {
+      return;
+    }
+
+    let theForm = event.target;
+
+    this.props.onRegisterUser(
+      form.username.value,
+      form.password.value,
+      form.email.value,
+      theForm.firstName.value,
+      theForm.lastName.value
+    );
+  };
+
   constructor() {
     super();
 
     this.first = '';
     this.last = '';
-    this.error = '';
-    this.unavailable = false;
     this.usernameManuallyChanged = false;
     this.state = {
       username: ''
     };
   }
 
-  submitHandler = (event) => {
-    event.preventDefault();
-
-    let form = event.target;
-
-    if (this.error) {
-      form.username.setCustomValidity(this.error);
-      return;
-    }
-
-    if (this.unavailable) {
-      return;
-    }
-
-    if (form.password.value != form.password_repeat.value) {
-      form.password_repeat.setCustomValidity("Passwords don't match");
-      return;
-    }
-
-    form.password_repeat.setCustomValidity('');
-
-    if (!form.agree.checked) {
-      form.agree.setCustomValidity('You have to agree to Terms before registering');
-      return;
-    }
-
-    this.props.onRegisterUser(
-      form.username.value,
-      form.password.value,
-      form.email.value,
-      form.firstName.value,
-      form.lastName.value
-    );
-  };
-
   async getAvailableUsername(username) {
     const client = new ApiClient(API_HOST);
     return await client.getAvailableUsername(username);
-  }
-
-  async checkUserExists(username) {
-    const client = new ApiClient(API_HOST);
-    return await client.checkUserExists(username);
-  }
-
-  async checkEmailTaken(email) {
-    const client = new ApiClient(API_HOST);
-    return await client.checkEmailTaken(email);
   }
 
   inputHandler = async (event) => {
@@ -128,72 +132,31 @@ export default class RegisterComponent extends React.Component {
 
     try {
       this.setState({ username: await this.getAvailableUsername(result) });
-      this.unavailable = false;
       this.error = '';
     } catch (e) {
       this.error = e.message;
     }
+
+    console.log(this.state.username);
   };
 
-  usernameInputHandler = async (event) => {
+  inputUsername = async (event) => {
     const result = event.target.value.replace(/\s|\W/g, '');
 
     this.setState({ username: result });
     this.usernameManuallyChanged = true;
-    
-    try {
-      this.unavailable = await this.checkUserExists(result);
-      this.error = '';
-    } catch (e) {
-      this.error = e.message;
-    }
-
-    if (this.unavailable) {
-      this.username.setCustomValidity('Username is taken');
-    } else {
-      this.username.setCustomValidity('');
-    }
-  };
-
-  emailValidation = async (event) => {
-    const result = event.target.value.trim();
-
-    let unavailable;
-    try {
-      unavailable = await this.checkEmailTaken(result);
-      this.error = '';
-    } catch (e) {
-      this.error = e.message;
-    }
-
-    if (unavailable) {
-      this.email.setCustomValidity('Email is taken');
-    } else {
-      this.email.setCustomValidity('');
-    }
-  }
-
-  passwordValidation = () => {
-    const pass = this.password;
-    const passRepeat = this.passwordRepeat;
-
-    if (!passRepeat.value || pass.value === passRepeat.value) {
-      pass.setCustomValidity('');
-      passRepeat.setCustomValidity('');
-    } else {
-      pass.setCustomValidity("Passwords don't match");
-      passRepeat.setCustomValidity("Passwords don't match");
-    }
   };
 
   render() {
+    const { fields, form } = this.props;
+
     if (this.props.registration_success) {
       ga('send', 'event', 'Reg', 'Done');
       return ( <SuccessContent onShowRegisterForm={this.props.onShowRegisterForm} /> );
     }
 
     const reset = ((e) => e.target.setCustomValidity(''));
-
+    console.log(fields.username);
     return (
     <div id="register" className="div">
       <header className="layout__row layout__row-double">
@@ -213,27 +176,42 @@ export default class RegisterComponent extends React.Component {
             <input onBlur={reset} onInput={this.inputHandler} className="input input-gray input-big input-block" type="text" placeholder="Lastname" id="registerLastName" name="lastName" />
           </div>
           <div className="layout__row layout__row-double">
-            <label className="label label-before_input" htmlFor="registerUsername">Username</label>
-            <input ref={(c) => this.username = c} onChange={this.usernameInputHandler} className="input input-gray input-big input-block" type="text" placeholder="Username" id="registerUsername" name="username" required="required" value={this.state.username} />
+            <label className="label label-before_input" htmlFor="username">Username</label>
+            <input ref={(c) => this.username = c} className="input input-gray input-big input-block" type="text" placeholder="Username" id="username" name="username" required="required" onChange={this.inputUsername} value={this.state.username} {...fields.username}/>
+            {fields.username.error &&
+              <Message message={fields.username.error} />
+            }
           </div>
           <div className="layout__row layout__row-double">
             <label className="label label-before_input" htmlFor="registerPassword">Password</label>
-            <input ref={(c) => this.password = c} onInput={this.passwordValidation} className="input input-gray input-big input-block" type="password" id="registerPassword" name="password" required="required" />
+            <input ref={(c) => this.password = c} onInput={this.passwordValidation} className="input input-gray input-big input-block" type="password" id="registerPassword" name="password" required="required" {...fields.password} />
+            {fields.password.error &&
+              <Message message={fields.password.error} />
+            }
           </div>
           <div className="layout__row layout__row-double">
             <label className="label label-before_input" htmlFor="registerPasswordRepeat">Repeat password</label>
-            <input ref={(c) => this.passwordRepeat = c} onInput={this.passwordValidation} className="input input-gray input-big input-block" type="password" id="registerPasswordRepeat" name="password_repeat" required="required" />
+            <input ref={(c) => this.passwordRepeat = c} onInput={this.passwordValidation} className="input input-gray input-big input-block" type="password" id="registerPasswordRepeat" name="password_repeat" required="required" {...fields.passwordRepeat} />
+            {fields.passwordRepeat.error &&
+              <Message message={fields.passwordRepeat.error} />
+            }
           </div>
           <div className="layout__row layout__row-double">
             <label className="label label-before_input label-space" htmlFor="registerEmail">Email</label>
-            <input ref={(c) => this.email = c} onChange={this.emailValidation} className="input input-gray input-big input-block" type="email" placeholder="email.address@example.com" id="registerEmail" name="email" required="required" />
+            <input ref={(c) => this.email = c} onChange={this.emailValidation} className="input input-gray input-big input-block" type="email" placeholder="email.address@example.com" id="registerEmail" name="email" required="required" {...fields.email} />
+            {fields.email.error &&
+              <Message message={fields.email.error} />
+            }
           </div>
         </div>
         <div className="layout__row layout__row-double">
+          {fields.agree.error &&
+            <Message message={fields.agree.error} />
+          }
           <div className="layout__grid layout__grid-big layout-align_vertical">
             <button className="button button-big button-green">Sign up</button>
             <label className="action checkbox">
-              <input name="agree" required="required" type="checkbox" />
+              <input name="agree" required="required" type="checkbox" {...fields.agree} />
               <span className="checkbox__label-right">I agree to Terms &amp; Conditions</span>
             </label>
           </div>
@@ -243,3 +221,98 @@ export default class RegisterComponent extends React.Component {
     )
   }
 }
+
+const checkEmailTaken = async (email) => {
+  const client = new ApiClient(API_HOST);
+  return await client.checkEmailTaken(email);
+};
+
+const checkUserExists = async (username) => {
+  const client = new ApiClient(API_HOST);
+  return await client.checkUserExists(username);
+};
+
+const validateUsername = async (username) => {
+  if (!username) {
+    return 'Enter your username';
+  }
+
+  let taken;
+  try {
+    taken = await checkUserExists(username);
+  } catch (e) {
+    return e.message;
+  }
+
+  if (taken) {
+    return 'Username is taken';
+  } else {
+    return undefined;
+  }
+};
+
+const validateEmail = async (email) => {
+  if (!email) {
+    return 'Enter your email address';
+  }
+
+  let taken;
+  try {
+    taken = await checkEmailTaken(email);
+  } catch (e) {
+    return e.message;
+  }
+
+  if (taken) {
+    return 'Email is taken';
+  } else {
+    return undefined;
+  }
+}
+
+const validatePassword = (password) => {
+  if (password && password.length < 8) {
+    return 'Password must contain at least 8 symbols';
+  } else {
+    return undefined;
+  }
+};
+
+const validatePasswordRepeat = (password, repeat) => {
+  if (password !== repeat) {
+    return "Passwords don't match";
+  } else {
+    return undefined;
+  }
+};
+
+const fields = ['username', 'password', 'passwordRepeat', 'email', 'agree'];
+const validate = async (values) => {
+  const {
+    username,
+    password,
+    passwordRepeat,
+    email,
+    agree
+  } = values;
+  const errors = {};
+
+  errors.username = await validateUsername(username);
+  errors.email = await validateEmail(email);
+  errors.password = validatePassword(password);
+  errors.passwordRepeat = validatePasswordRepeat(password, passwordRepeat);
+  
+  if (!agree) {
+    errors.agree = 'You have to agree to Terms before registering';
+  }
+
+  return errors;
+};
+
+const WrappedRegister = inform({
+  fields,
+  validate
+})(Register);
+
+export default WrappedRegister;
+
