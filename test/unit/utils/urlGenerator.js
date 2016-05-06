35d252695b1f7d5438f4a1b661ddbac9f4d45523
Alexander Pletnev <pletnev.rusalex@gmail.com>
2016-05-06 07:27:36 +0000

    test urlGenerator

---
+++ test/unit/utils/urlGenerator.js
@@ -0,0 +1,11 @@
+/*eslint-env node, mocha */
+import { expect } from '../../../test-helpers/expect-unit';
+import { getApiUrl } from '../../../src/utils/urlGenerator';
+
+describe('urlGenerator', () => {
+
+  it('Should work well ', () => {
+
+    expect(getApiUrl('USERS'), 'to equal', 'http://localhost:8000/api/v1/users');
+  });
+});
