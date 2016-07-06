3967656a9da4be8b008f1b1c710d2cabd738810b
Oopscurity <headlessfirst@gmail.com>
2016-07-06 14:23:53 +0300

    PropTypes/utils: createSimplifiedRequirableTypeChecker, error getters

--- src/prop-types/utils.js
+++ src/prop-types/utils.js
@@ -15,6 +15,19 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+export function getNotFoundError(propFullName, componentName, location) {
+  return new Error(
+    `Required ${location} \`${propFullName}\` was not specified in \`${componentName}\``
+  );
+}
+
+export function getTypeError(obj, expectedType, fullName, componentName, location) {
+  return new Error(
+    `Invalid ${location} \`${fullName}\` of type \`${typeof obj}\` ` +
+    `supplied to \`${componentName}\`, expected \`${expectedType}\``
+  );
+}
+
 export function createRequirableTypeChecker(validate) {
   function checkType(isRequired, props, propName, componentName, location, propFullName) {
     let fullName = propName;
@@ -24,15 +37,38 @@ export function createRequirableTypeChecker(validate) {
 
     if (props[propName] == null) {
       if (isRequired) {
-        return new Error(
-          `Required ${location} \`${fullName}\` was not specified in \`${componentName}\``
-        );
+        return getNotFoundError(fullName, componentName, location);
       }
 
       return null;
     }
 
     return validate(props, propName, componentName, location, fullName);
+  }
+
+  const chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+}
+
+export function createSimplifiedRequirableTypeChecker(validate) {
+  function checkType(isRequired, props, propName, componentName, location, propFullName) {
+    let fullName = propName;
+    if (propFullName) {
+      fullName = propFullName;
+    }
+
+    const propValue = props[propName];
+    if (propValue == null) {
+      if (isRequired) {
+        return getNotFoundError(fullName, componentName, location);
+      }
+
+      return null;
+    }
+
+    return validate(propValue, fullName, componentName, location);
   }
 
   const chainedCheckType = checkType.bind(null, false);
