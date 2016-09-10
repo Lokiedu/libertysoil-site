6ff506bd43045a3ba41c1bc3bd3750674a8f50b0
Alexander Pletnev <pletnev.rusalex@gmail.com>
2016-09-10 16:39:16 +0300

    integration/pages/school not to WARN
    school

--- src/components/follow-tag-button.js
+++ src/components/follow-tag-button.js
@@ -101,17 +101,21 @@ export default class FollowTagButton extends React.Component {
       return null;
     }
 
+    const buttonProps = Object.assign({}, props);
+    delete buttonProps.tag;
+    delete buttonProps.triggers;
+
     // If followTag or unfollowTag was performed
     if (status !== STATUS_NOT_TOUCHED) {
       switch (status) {
         case STATUS_FOLLOWING:
-          return <button {...props} className={`button button-green ${className}`} type="button">Following...</button>;
+          return <button {...buttonProps} className={`button button-green ${className}`} type="button">Following...</button>;
         case STATUS_UNFOLLOWING:
-          return <button {...props} className={`button button-green ${className}`} type="button">Unfollowing...</button>;
+          return <button {...buttonProps} className={`button button-green ${className}`} type="button">Unfollowing...</button>;
         case STATUS_JUST_FOLLOWED:
-          return <button {...props} className={`button button-yellow ${className}`} type="button" onClick={this._unfollowTag}>Followed!</button>;
+          return <button {...buttonProps} className={`button button-yellow ${className}`} type="button" onClick={this._unfollowTag}>Followed!</button>;
         case STATUS_JUST_UNFOLLOWED:
-          return <button {...props} className={`button button-green ${className}`} type="button" onClick={this._followTag}>Unfollowed!</button>;
+          return <button {...buttonProps} className={`button button-green ${className}`} type="button" onClick={this._followTag}>Unfollowed!</button>;
         default:
           return null;
       }
@@ -119,9 +123,9 @@ export default class FollowTagButton extends React.Component {
       const isFollowed = !!followed_tags[this.props.tag];
 
       if (isFollowed) {
-        return <button {...props} className={`button button-yellow ${className}`} type="button" onClick={this._unfollowTag}>Following</button>;
+        return <button {...buttonProps} className={`button button-yellow ${className}`} type="button" onClick={this._unfollowTag}>Following</button>;
       } else {  // eslint-disable-line no-else-return
-        return <button {...props} className={`button button-red ${className}`} type="button" onClick={this._followTag}>Follow</button>;
+        return <button {...buttonProps} className={`button button-red ${className}`} type="button" onClick={this._followTag}>Follow</button>;
       }
     }
   }
