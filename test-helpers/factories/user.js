8e08481b0560902c1021ddb4a47969c94fd975ac
Alexey Zakhlestin <indeyets@gmail.com>
2016-09-23 15:38:53 +0300

    Test that "Follow" button shows its 2 states
    refs #641

--- test-helpers/factories/user.js
+++ test-helpers/factories/user.js
@@ -6,8 +6,11 @@ const faker = require('faker');
 
 export default new Factory()
   .attr('id', () => uuid.v4())
+  .attr('created_at', () => faker.date.recent())
+  .attr('updated_at', () => faker.date.recent())
   .attr('username', () => faker.internet.userName().toLowerCase())
   .attr('email', () => faker.internet.email())
   .attr('password', () => faker.internet.password())
   .attr('hashed_password', ['password'], password => bcrypt.hashSync(password, 10))
-  .attr('email_check_hash', '');
+  .attr('email_check_hash', '')
+  .attr('more', { first_login: false });
--- test/unit/components/follow-button.js
+++ test/unit/components/follow-button.js
@@ -1,15 +1,57 @@
 /*eslint-env node, mocha */
 import { shallow } from 'enzyme';
+import { v4 as uuid4 } from 'uuid';
 
-import { TestUtils, unexpected, expect, React } from '../../../test-helpers/expect-unit';
+import { expect, React } from '../../../test-helpers/expect-unit';
 import FollowButton from '../../../src/components/follow-button';
+import userFactory from '../../../test-helpers/factories/user';
 
 
-describe('Follow button Test', function() {
+describe('Follow button Test', function () {
+  const currentUser = userFactory.build();
+  currentUser.favourites = [];
+  currentUser.geotags = [];
+  currentUser.hashtags = [];
+  currentUser.schools = [];
+  currentUser.suggested_users = [];
+  currentUser.followed_geotags = [];
+  currentUser.followed_hashtags = [];
+  currentUser.followed_schools = [];
+  currentUser.liked_geotags = [];
+  currentUser.liked_hashtags = [];
+  currentUser.liked_schools = [];
+  currentUser.likes = [];
+  currentUser.recent_tags = {
+    geotags: [],
+    hashtags: [],
+    schools: []
+  };
 
-  it('is empty for anonym user or when no current_user in props', function() {
+  it('is empty for anonym user or when no current_user in props', function () {
     const wrapper = shallow(<FollowButton />);
 
     return expect(wrapper.equals(null), 'to be true');
+  });
+
+  it('should render "Follow" for unfollowed users', () => {
+    const props = {
+      active_user: currentUser,
+      following: [uuid4(), uuid4()],
+      user: userFactory.build()
+    };
+    const wrapper = shallow(<FollowButton {...props} />);
+    return expect(wrapper.text(), 'to be', 'Follow');
+  });
+
+  it('should render "Following" for followed users', () => {
+    const user = userFactory.build();
+
+    const props = {
+      active_user: currentUser,
+      following: [uuid4(), uuid4(), user.id],
+      user
+    };
+    const wrapper = shallow(<FollowButton {...props} />);
+    return expect(wrapper.text(), 'to be', 'Following');
   });
 });
