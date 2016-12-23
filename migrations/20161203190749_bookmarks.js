e9d731f11f477e02c75d992d9fe9ab63eb207f0e
Artem Kravchenko <kravchenkofirst@gmail.com>
2016-12-23 15:38:08 +0300

    Create bookmarks table

--- migrations/20161203190749_bookmarks.js
+++ migrations/20161203190749_bookmarks.js
@@ -4,9 +4,12 @@ export async function up(knex, Promise) {
     table.uuid('user_id').references('id').inTable('users').onDelete('cascade').onUpdate('cascade');
     table.text('url');
     table.text('title');
+    table.integer('ord').notNullable();
     table.jsonb('more');
     table.timestamp('created_at', true).defaultTo(knex.raw("(now() at time zone 'utc')"));
     table.timestamp('updated_at', true).defaultTo(knex.raw("(now() at time zone 'utc')"));
+
+    table.unique(['user_id', 'ord']);
   });
 }
 
