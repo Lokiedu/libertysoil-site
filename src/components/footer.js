2381186baedf630bd02a50654959b4bd2b96d3a7
Alexey Zakhlestin <indeyets@gmail.com>
2017-02-13 22:20:33 +0300

    Bump version

--- src/components/footer.js
+++ src/components/footer.js
@@ -31,7 +31,7 @@ const FooterComponent = () => (
       <a href="mailto:info@libertysoil.org">info@libertysoil.org</a>
     </p>
     <p>
-      <a href="https://github.com/Lokiedu/libertysoil-site/milestones">Version 1.9.1</a>
+      <a href="https://github.com/Lokiedu/libertysoil-site/milestones">Version 1.9.2</a>
     </p>
     <script async data-trackduck-id="56182f159e7749be13765442" src="//d1ks1friyst4m3.cloudfront.net/toolbar/prod/td.js" />
   </footer>
