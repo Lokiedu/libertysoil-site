388d7fad30c111fb26ce71fa5e541f73bb3506a5
Dmitry Vdovin <voidxnull@gmail.com>
2017-02-15 00:15:02 +0200

    [#790] Add CRUD controllers for profile posts

--- src/api/controller.js
+++ src/api/controller.js
@@ -3342,7 +3342,122 @@ export default class ApiController {
     ctx.body = messages;
   }
 
+  getProfilePosts = async (ctx) => {
+    const User = this.bookshelf.model('User');
+
+    try {
+      const user = await new User({ username: ctx.params.username }).fetch({ require: true });
+      const posts = await user.profile_posts().fetch();
+
+      ctx.body = posts;
+    } catch (e) {
+      this.processError(ctx, e);
+    }
+  }
+
+  getProfilePost = async (ctx) => {
+    const ProfilePost = this.bookshelf.model('ProfilePost');
+
+    try {
+      const post = await new ProfilePost({ id: ctx.params.id })
+        .fetch({ require: true });
+
+      ctx.body = post;
+    } catch (e) {
+      this.processError(ctx, e);
+    }
+  }
+
+  createProfilePost = async (ctx) => {
+    if (!ctx.session || !ctx.session.user) {
+      ctx.status = 403;
+      ctx.body = { error: 'You are not authorized' };
+      return;
+    }
+
+    const ProfilePost = this.bookshelf.model('ProfilePost');
+
+    const postAttrs = _.pick(ctx.request.body, ['text', 'type', 'more']);
+    postAttrs.user_id = ctx.session.user;
+
+    const post = new ProfilePost(postAttrs);
+
+    try {
+      await post.save();
+      ctx.body = await post.fetch();
+    } catch (e) {
+      this.processError(ctx, e);
+    }
+  }
+
+  updateProfilePost = async (ctx) => {
+    if (!ctx.session || !ctx.session.user) {
+      ctx.status = 403;
+      ctx.body = { error: 'You are not authorized' };
+      return;
+    }
+
+    const ProfilePost = this.bookshelf.model('ProfilePost');
+
+    try {
+      const post = await new ProfilePost()
+        .where({ id: ctx.params.id, user_id: ctx.session.user })
+        .fetch({ require: true });
+      await post.save(_.pick(ctx.request.body, ['text', 'type', 'more']), { patch: true });
+
+      ctx.body = await post.refresh();
+    } catch (e) {
+      this.processError(ctx, e);
+    }
+  }
+
+  deleteProfilePost = async (ctx) => {
+    const ProfilePost = this.bookshelf.model('ProfilePost');
+
+    try {
+      await new ProfilePost({ id: ctx.params.id, user_id: ctx.session.user })
+        .destroy({ require: true });
+
+      ctx.body = {
+        success: true
+      };
+    } catch (e) {
+      this.processError(ctx, e);
+    }
+  }
+
   // ========== Helpers ==========
+
+  /**
+   * Sets the response body in case of error
+   * ```
+   * {
+   *   errors: { fieldName: ['Some message']] }, // Only if e is a Checkit.Error
+   *   error: 'Human readable message'
+   * }
+   * ```
+   * @param ctx Koa context
+   * @param {Error} e
+   */
+  processError(ctx, e) {
+    ctx.status = 500;
+
+    if (e instanceof Checkit.Error) {
+      ctx.body = {
+        errors: e.toJSON(),
+        error: e.toString()
+      };
+    } else if (e instanceof this.bookshelf.NotFoundError) {
+      ctx.status = 404;
+      ctx.body = {
+        error: 'Not Found'
+      };
+    } else {
+      ctx.body = {
+        error: e.message
+      };
+    }
+  }
 
   async areMutuallyFollowed(user1Id, user2Id) {
     const knex = this.bookshelf.knex;
