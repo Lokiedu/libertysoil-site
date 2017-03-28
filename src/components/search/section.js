b05a42e34bc63332b2dcc171de3e445e9e6c8634
Artyom Kravchenko <kravchenkofirst@gmail.com>
2017-03-28 16:24:14 +0300

    Add support of new offset-based paging

--- src/components/search/section.js
+++ src/components/search/section.js
@@ -105,15 +105,11 @@ export default class SearchSection extends React.Component {
       return null;
     }
 
-    console.log(type);
-
     let itemsToRender;
     if (needPaging) {
-      itemsToRender = items
-        .slice(offset, offset + SEARCH_RESULTS_PER_PAGE);
+      itemsToRender = items;
     } else {
-      itemsToRender = items
-        .slice(offset, offset + SEARCH_RESULTS_PER_PAGE / 2);
+      itemsToRender = items.take(SEARCH_RESULTS_PER_PAGE / 2);
     }
 
     const rendered = this.renderItems(type, itemsToRender, props);
