b68229e149a397e32fed441612a9e5d8ebf3b68d
Dmitry Vdovin <voidxnull@gmail.com>
2018-01-08 01:14:15 +0200

    Refactor email template tests

--- test/unit/email-templates/email-templates.js
+++ test/unit/email-templates/email-templates.js
@@ -1,97 +1,160 @@
 /*eslint-env node, mocha */
+import faker from 'faker';
+import { JSDOM } from 'jsdom';
 import { API_HOST } from '../../../src/config';
 import { expect } from '../../../test-helpers/expect-unit';
-import { renderVerificationTemplate, renderWelcomeTemplate, renderNewCommentTemplate }  from '../../../src/email-templates/index';
+import { EmailTemplates } from '../../../src/email-templates';
 
 describe('Email templates:', function () {
-  it('Verification template exist', async function () {
-    const template = await renderVerificationTemplate(new Date());
+  const templates = new EmailTemplates();
 
-    expect(template, 'to be a string');
-  });
-
-  it('Welcome template exist', async function () {
-    const template = await renderWelcomeTemplate(new Date());
-
-    expect(template, 'to be a string');
-  });
-
-  describe('#renderNewCommentTemplate', function () {
-    const comment = {
-      text: 'Test comment text',
-      post_id: 1
-    };
-
-    const commentAuthor = {
-      username: 'JohnDoe',
-      more: {
-        firstName: 'John',
-        lastName: 'Doe',
-        avatar: {
-          url: 'http://avatars.test/avatar.png'
-        }
-      }
-    };
-
+  describe('new-comment.ejs', function () {
     const post = {
-      id: 1,
+      id: '99e79372-2958-4ecb-8314-aa56a3dad326',
       more: {
-        pageTitle: 'Hello world!'
-      }
+        pageTitle: 'Post Title'
+      },
+      user: {
+        username: 'post_author',
+        gravatarHash: '123'
+      },
+      comments: [
+        {
+          text: 'Comment 1 text',
+          created_at: new Date(2018, 0, 0).toJSON(),
+          user: {
+            fullName: 'Comment Author',
+            username: 'comment_author',
+            gravatarHash: '123'
+          },
+        },
+      ]
     };
 
-    const postAuthor = {
-      more: {
-        avatar: {
-          url: 'http://avatars.test/avatar2.png'
-        }
-      }
-    };
-
-    let template;
+    let document;
 
     before(async function () {
-      template = await renderNewCommentTemplate(comment, commentAuthor, post, postAuthor);
+      const template = await templates.renderNewCommentTemplate({ post });
+      document = (new JSDOM(template)).window.document;
     });
 
-    it('renders link to the author', function () {
+    it('renders link to author', function () {
       expect(
-        template,
-        'when parsed as HTML',
+        document.body,
         'queried for first', '#comment-author-link',
-        'to have attributes', { href: `${API_HOST}/user/${commentAuthor.username}` }
+        'to have attributes', { href: `${API_HOST}/user/${post.comments[0].user.username}` }
       );
 
       expect(
-        template,
-        'when parsed as HTML',
+        document.body,
         'queried for first', '#comment-author-link',
-        'to have text', 'John Doe'
+        'to have text', 'Comment Author'
       );
     });
 
-    it('renders link to the post', function () {
+    it('renders link to post', function () {
       expect(
-        template,
-        'when parsed as HTML',
+        document.body,
         'queried for first', '#post-link',
         'to have attributes', { href: `${API_HOST}/post/${post.id}` }
       );
 
       expect(
-        template,
-        'when parsed as HTML',
+        document.body,
         'queried for first', '#post-link',
         'to have text', post.more.pageTitle
       );
     });
 
-    it('renders the comment', function () {
+    it('renders comment', () => {
       expect(
-        template,
-        'when parsed as HTML',
-        'queried for first', '.page__content_text-comment',
-        'to have text', comment.text
+        document.body,
+        'queried for first', '.post_comment',
+        'to satisfy',
+        expect.it('to have text', expect.it('to contain', 'Comment 1 text'))
+      );
+    });
+  });
+
+  describe('new-comments.ejs', function () {
+    const posts = [
+      {
+        id: '99e79372-2958-4ecb-8314-aa56a3dad326',
+        more: {
+          pageTitle: 'Post 1'
+        },
+        user: {
+          fullName: faker.name.findName(),
+          gravatarHash: '123'
+        },
+        comments: [
+          {
+            text: 'Post 1 Comment 1 text',
+            created_at: new Date(2018, 0, 0).toJSON(),
+            user: {
+              fullName: faker.name.findName(),
+              gravatarHash: '123'
+            },
+          },
+          {
+            text: 'Post 1 Comment 2 text',
+            created_at: new Date(2018, 0, 0).toJSON(),
+            user: {
+              fullName: faker.name.findName(),
+              gravatarHash: '123'
+            },
+          }
+        ]
+      },
+      {
+        id: '99e79372-2958-4ecb-8314-aa56a3dad326',
+        more: {
+          pageTitle: 'Post 2'
+        },
+        user: {
+          name: 'John Doe',
+          gravatarHash: '123'
+        },
+        comments: [
+          {
+            text: 'Post 2 Comment 1 text',
+            created_at: new Date(2018, 0, 0).toJSON(),
+            user: {
+              name: 'Commenter',
+              gravatarHash: '123'
+            },
+          }
+        ]
+      },
+    ];
+
+    let document;
+
+    before(async function () {
+      const template = await templates.renderNewCommentsTemplate({ posts, since: faker.date.past() });
+      document = (new JSDOM(template)).window.document;
+    });
+
+    it('renders all comments', () => {
+      expect(
+        document.body,
+        'queried for', '.post_comment',
+        'to satisfy', [
+          expect.it('to have text', expect.it('to contain', 'Post 1 Comment 1 text')),
+          expect.it('to have text', expect.it('to contain', 'Post 1 Comment 2 text')),
+          expect.it('to have text', expect.it('to contain', 'Post 2 Comment 1 text')),
+        ]
+      );
+    });
+
+    it('renders multiple posts', () => {
+      expect(
+        document.body,
+        'queried for', '.post',
+        'to satisfy', [
+          expect.it('to have text', expect.it('to contain', 'Post 1')),
+          expect.it('to have text', expect.it('to contain', 'Post 2')),
+        ]
       );
     });
   });
